227aa01c233fde4df4a996cae4fbe27040adc582
Sharif Parish <saintparish6@gmail.com>
2025-03-17 23:39:31 -0400

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,12 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const prisma = global.prisma || new PrismaClient();
+// Use a singleton in development to avoid new connections
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
