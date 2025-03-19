e81a6f7d260ef91a50efc5e3c2e9c5aeba5a9312
Sharif <saintparish6@gmail.com>
2025-03-19 08:50:31 -0400

    Prisma lib fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
+// eslint-disable-next-line no-var
 declare global {
-  // allow global 'prisma' variable
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = globalThis.prisma || new PrismaClient();
+const prisma = globalThis.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma;
