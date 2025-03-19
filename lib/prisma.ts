81d3f8f658181065d56ffdcb5bab6a8e0a9da906
Sharif <saintparish6@gmail.com>
2025-03-19 08:55:41 -0400

    Alt solution

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-// eslint-disable-next-line no-var
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+type GlobalPrisma = {
+  prisma?: PrismaClient;
+};
 
-const prisma = globalThis.prisma ?? new PrismaClient();
+const globalForPrisma = globalThis as GlobalPrisma;
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma;
+  globalForPrisma.prisma = prisma;
 }
 
 export default prisma;
